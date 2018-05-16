9403a1370d9e7e2da5ca3545591b38da25b78c85
Kim Hyemin <beigenut@gmail.com>
2018-05-16 15:17:08 +0900

    Apply Morgan

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+// morgan : terminal 에 log (text) 가 나올 수 있도록 하는 express middleware
+var morgan = require('morgan')
 
 const app = express()
 
+app.use(morgan('dev'))
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
